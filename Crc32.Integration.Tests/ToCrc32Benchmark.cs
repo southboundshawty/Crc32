78fb07a047abfa18c61ca535dfa5ff7a0cb730aa
southboundshawty <stevemine71@gmail.com>
2023-01-22 11:50:24 +0300

    .

--- Crc32.Integration.Tests/ToCrc32Benchmark.cs
+++ Crc32.Integration.Tests/ToCrc32Benchmark.cs
@@ -5,7 +5,7 @@ namespace Crc32.Integration.Tests;
 [MemoryDiagnoser]
 public class ToCrc32Benchmark
 {
-    [Params("Hello World", "The quick brown fox jumps over the lazy dog", "", null)]
+    [Params("Hello World", "The quick brown fox jumps over the lazy dog", "")]
     public string Input { get; set; }
 
     [Benchmark]
--- Crc32.Unit.Tests/Crc32ExtensionsTests.cs
+++ Crc32.Unit.Tests/Crc32ExtensionsTests.cs
@@ -5,17 +5,14 @@ namespace Crc32.Unit.Tests;
 public class Crc32ExtensionsTests
 {
     [Theory]
-    [InlineData("hello", "185f9656")]
-    [InlineData("world", "1cbf0835")]
+    [InlineData("hello", "3610a686")]
+    [InlineData("world", "3a771143")]
     [InlineData("", "00000000")]
-    [InlineData("xunit", "b0f9c966")]
-    [InlineData("fluent", "2f2b7c3b")]
-    [InlineData("inline", "9a9a1a2a")]
     public void ToCrc32_ShouldReturnExpectedValue(string input, string expected)
     {
         // Act
         var result = input.ToCrc32();
-
+        
         // Assert
         Assert.Equal(expected, result);
     }
